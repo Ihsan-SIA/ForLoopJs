2f5b115f5f563d1086408f7ad2f20d48059cb625
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-22 15:16:13 +0100

    Practice on for loop for javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+// for (let index = 0; index <= 50; index++) {
+//     console.log(index);
+    
+// }
+console.log(document.getElementsByClassName('text'));
+console.log(document.getElementById);
+console.log(document.getElementById('newButton'));
